bcd90fbdc108da06804e2e43b7a13a0c9676d867
Greg Hudy <greg.hudy@gmail.com>
2013-10-01 11:29:02 -0500

    Update EmailComposer.js
    Edited to allow for filenames in attachments

--- iOS/EmailComposerWithAttachments/EmailComposer.js
+++ iOS/EmailComposerWithAttachments/EmailComposer.js
@@ -1,60 +1,72 @@
-// window.plugins.emailComposer
+/**
+ *
+ * Email Composer plugin for PhoneGap/Cordova
+ * window.plugins.emailComposer
+ *
+ * Unified and updated API to be cross-platform by Gal Cohen in 2013.
+ * galcohen26@gmail.com
+ * https://github.com/GalCohen
+ *
+ * Original code from:
+ * android: https://github.com/phonegap/phonegap-plugins/tree/master/Android/EmailComposerWithAttachments
+ * ios https://github.com/phonegap/phonegap-plugins/tree/5cf45fcade4989668e95a6d34630d2021c45291a/iOS/SMSComposer
+ * js: https://github.com/phonegap/phonegap-plugins/blob/5cf45fcade4989668e95a6d34630d2021c45291a/iOS/SMSComposer/SMSComposer.js
+ *
+ */
 
-function EmailComposer() {
-	this.resultCallback = null; // Function
-}
 
-EmailComposer.ComposeResultType = {
-Cancelled:0,
-Saved:1,
-Sent:2,
-Failed:3,
-NotSent:4
-}
+/*
+ * Temporary Scope to contain the plugin.
+ */
+(function() {
+     /* Get local ref to global PhoneGap/Cordova/cordova object for exec function.
+      - This increases the compatibility of the plugin. */
+     var cordovaRef = window.PhoneGap || window.Cordova || window.cordova; // old to new fallbacks
+     
+    function EmailComposer() {
+        this.resultCallback = null; // Function
+    }
 
+    EmailComposer.ComposeResultType = {
+        Cancelled:0,
+        Saved:1,
+        Sent:2,
+        Failed:3,
+        NotSent:4
+    }
 
+    // showEmailComposer : all args optional
+    EmailComposer.prototype.showEmailComposer = function(subject, body, toRecipients, ccRecipients, bccRecipients, bIsHTML, attachments, fileNames) {
+        var args = {};
+        args.subject = subject ? subject : "";
+        args.body = body ? body : "";
+        args.toRecipients = toRecipients ? toRecipients : [];
+        args.ccRecipients = ccRecipients ? ccRecipients : [];
+        args.bccRecipients = bccRecipients ? bccRecipients : [];
+        args.bIsHTML = bIsHTML ? true : false;
+        args.attachments = attachments ? attachments : [];
+        //added for fileNames
+        args.fileNames = fileNames ? fileNames : [];
+        cordovaRef.exec(null, null, "EmailComposer", "showEmailComposer", [args]);
+    }
 
-// showEmailComposer : all args optional
+    EmailComposer.prototype.showEmailComposerWithCallback = function(callback, subject, body, toRecipients, ccRecipients, bccRecipients, isHTML, attachments) {
+        this.resultCallback = callback;
+        this.showEmailComposer.apply(this, [subject, body, toRecipients, ccRecipients, bccRecipients, isHTML, attachments]);
+    }
 
-EmailComposer.prototype.showEmailComposer = function(subject,body,toRecipients,ccRecipients,bccRecipients,bIsHTML,attachments) {
-	var args = {};
-	if(toRecipients)
-		args.toRecipients = toRecipients;
-	if(ccRecipients)
-		args.ccRecipients = ccRecipients;
-	if(bccRecipients)
-		args.bccRecipients = bccRecipients;
-	if(subject)
-		args.subject = subject;
-	if(body)
-		args.body = body;
-	if(bIsHTML)
-		args.bIsHTML = bIsHTML;
-    if(attachments)
-        args.attachments = attachments;
-	
-	cordova.exec(null, null, "EmailComposer", "showEmailComposer", [args]);
-}
+    EmailComposer.prototype._didFinishWithResult = function(res) {
+        this.resultCallback(res);
+    }
 
-EmailComposer.prototype.showEmailComposerWithCallback = function(callback, subject, body, toRecipients, ccRecipients, bccRecipients, isHTML, attachments) {
-	this.resultCallback = callback;
-	this.showEmailComposer.apply(this,[subject,body,toRecipients,ccRecipients,bccRecipients,isHTML,attachments]);
-}
-
-EmailComposer.prototype._didFinishWithResult = function(res) {
-	this.resultCallback(res);
-}
-
-cordova.addConstructor(function()  {
-					   if(!window.plugins)
-					   {
-					   window.plugins = {};
-					   }
-					   
-					   // shim to work in 1.5 and 1.6
-					   if (!window.Cordova) {
-					   window.Cordova = cordova;
-					   };
-					   
-					   window.plugins.emailComposer = new EmailComposer();
-					   });
+    cordovaRef.addConstructor(function() {
+           if (!window.plugins) {
+                window.plugins = {};
+           }
+                                           
+            if (!window.plugins.emailComposer) {
+               window.plugins.emailComposer = new EmailComposer();
+                console.log("**************************** Email Composer ready *************************");
+            }
+        });
+})();/* End of Temporary Scope. */
